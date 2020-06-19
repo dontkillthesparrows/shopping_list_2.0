4781b43d3af19f054f4af1b9166ee94d036f231d
dontkillthesparrows <post@althea.xyz>
2020-06-19 10:59:46 +0200

    basic rework

---
+++ src/helperfunctions.js
@@ -0,0 +1,27 @@
+export function storageAvaliable(type) {
+  //function from MDN, using web storage API
+  let storage;
+  try {
+    storage = window[type];
+    let x = '_storage_test_';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (error) {
+    return (
+      error instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
---
+++ src/script.js
@@ -0,0 +1,61 @@
+import { storageAvaliable } from './helperfunctions.js';
+
+const input = document.getElementById('inputField');
+const button = document.getElementById('submitButton');
+const ul = document.getElementById('list');
+
+window.onload = init;
+
+function init() {
+  if (storageAvaliable) {
+    console.log('storage avaliable');
+  } else {
+    console.log('storage not avaliable');
+  }
+}
+
+function appendListElement(id, value) {
+  ul.insertAdjacentHTML(
+    'beforeend',
+    `<li id="${id}" class="list-item">${value}<span class="check-box"></span><span class="recycle">&#9850;</span></li>`
+  );
+}
+
+function removeListElement(element) {
+  element.parentElement.parentNode.removeChild(element.parentElement);
+}
+
+function addItemToLocalStorage(key, value) {
+  localStorage.setItem(
+    key,
+    JSON.stringify({ key: key, value: value, done: false })
+  );
+}
+
+function removeItemFromLocalStorage(key) {
+  localStorage.removeItem(key);
+}
+
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (input.value) {
+    const key = Date.now();
+    appendListElement(key, input.value);
+    addItemToLocalStorage(key, input.value);
+  }
+
+  input.value = '';
+});
+
+document.addEventListener(
+  'click',
+  function (e) {
+    if (!e.target.matches('.recycle')) return;
+    e.preventDefault();
+    removeListElement(e.target);
+    console.log(e.target.parentElement.id);
+    removeItemFromLocalStorage(e.target.parentElement.id);
+  },
+  false
+);
